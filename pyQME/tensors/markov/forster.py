fixed bug in modified redfield forster dephasing and implemented easier calculation of complex Redfield dephasing without the use of the tensor or the Gamma

pyQME/tensors/markov/forster.py
@@ -89,7 +89,7 @@ class ForsterTensor(RelTensorMarkov):
             rates[np.diag_indices_from(rates)] = 0.0
             rates[np.diag_indices_from(rates)] = -np.sum(rates,axis=0)
 
-            self.rates = self.transform(rates)
+            self.rates = rates #self.transform(rates)
 
     
     def _calc_tensor(self):

pyQME/tensors/markov/modified_redfield_forster.py
@@ -185,9 +185,9 @@ def MRF_rates_loop(Om,V_exc,redf_dephasing,time_axis,g_aabb,reorg_aabb,gdot_abbb
                 #D-->A rate
 
                 # GENERALIZED-FORSTER TERM
-                energy = Om[A,D]+2*ReorgD
+                energy = 1j*Om[A,D]+1j*2*ReorgD+redf_dephasing[D].conj()+redf_dephasing[A]
                 lineshape_function = gD+gA
-                exponent = 1j*energy*time_axis+lineshape_function+redf_dephasing[D].conj()+redf_dephasing[A]
+                exponent = energy*time_axis+lineshape_function
                 exponent = exponent - 2*(g_aabb[A,D]+1j*time_axis*reorg_aabb[A,D])
                 spectral_overlap_time = np.exp(-exponent)
                 integrand = 2. * ((V_exc[D,A]/h_bar)**2)*spectral_overlap_time.real
@@ -211,8 +211,7 @@ def MRF_rates_loop(Om,V_exc,redf_dephasing,time_axis,g_aabb,reorg_aabb,gdot_abbb
                 # YANG TERM
                 if include_yang_term:
                     square_brakets = 2*(gdot_abbb[A,D] - gdot_abbb[D,A] - 2*1j*reorg_aaab[A,D])
-                    integrand = integrand + 2*V_exc[D,A]*(spectral_overlap_time*square_brakets).imag    
-
+                    integrand = integrand + 2*V_exc[D,A]*(spectral_overlap_time*square_brakets).imag
                 rates[D,A] = np.trapz(integrand,time_axis)
     #fix diagonal
     rates[np.diag_indices_from(rates)] = 0.0

pyQME/tensors/markov/redfield.py
@@ -164,22 +164,26 @@ class RedfieldTensor(RelTensorMarkov):
 
         #case 2: the full GammF tensor is not available, but we cannot use the rates because they are real --> let's compute the dephasing
         else:
+            cc = self.U
+            dephasing = np.zeros([self.dim],dtype=np.complex128)
             SD_id_list = self.SD_id_list
 
             #loop over the redundancies-free list of spectral densities
             for SD_idx,SD_id in enumerate([*set(SD_id_list)]):
 
                 Cw_matrix = self.specden(self.Om.T,SD_id=SD_id,imag=True)
-
+                
                 mask = [chrom_idx for chrom_idx,x in enumerate(SD_id_list) if x == SD_id]
-                if SD_idx == 0:
-                    GammF_aaaa  = contract('iaa,iaa,aa->a',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
-                    GammF_abba =  contract('iab,iba,ba->ab',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
-                else:
-                    GammF_aaaa = GammF_aaaa + contract('iaa,iaa,aa->a',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
-                    GammF_abba = GammF_abba + contract('iab,iba,ba->ab',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
+                dephasing += contract('ia,ib,ba,ab->a',cc[mask,:]**2,cc[mask,:]**2,Cw_matrix,1-np.eye(self.dim))/2
 
-            dephasing = -0.5*(GammF_aaaa - contract('ab->a',GammF_abba))
+#                 if SD_idx == 0:
+#                     GammF_aaaa  = contract('iaa,iaa,aa->a',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
+#                     GammF_abba =  contract('iab,iba,ba->ab',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
+#                 else:
+#                     GammF_aaaa = GammF_aaaa + contract('iaa,iaa,aa->a',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
+#                     GammF_abba = GammF_abba + contract('iab,iba,ba->ab',self.X[mask,:,:],self.X[mask,:,:],Cw_matrix)
+
+#             dephasing = -0.5*(GammF_aaaa - contract('ab->a',GammF_abba))
             
         return dephasing
 

pyQME/tensors/relaxation_tensor.py
@@ -835,16 +835,17 @@ class RelTensorMarkov(RelTensor):
 
         #for fluorescence spectra we need adiabatic equilibrium population, so we subtract the reorganization energy
         ene = self.ene.copy()
+        #ene = ene + 1j*0
         if include_lamb:
             ene -= self.get_lambda_a()
         if include_deph:
             ene += self.get_dephasing().imag
-        
+            #ene -= 1j*self.get_dephasing().real
         #we scale the energies to avoid numerical difficulties
         ene -= ene.min()
         
         boltz = np.exp(-ene*self.specden.beta)
-        
+
         #the populations are not normalized because the normalization must be done taking into account also of dipoles, which is managed by the SpectraCalculator 
         if normalize:
             boltz = boltz/boltz.sum()

pyQME/utils.py
@@ -683,7 +683,7 @@ def calc_spec_localized_vib(SDobj_delocalized,SDobj_localized,H,dipoles,rel_tens
     reorg_high_list = np.asarray([SDobj_localized.Reorg[SD_idx] for SD_idx in SD_id_list])
     exp = np.exp(-0.5*HR_high_list)
     dipoles_low = dipoles*exp[:,np.newaxis]
-    dipoles_high = dipoles*np.sqrt((1 - exp[:,np.newaxis]**2))    
+    #dipoles_high = dipoles*np.sqrt((1 - exp[:,np.newaxis]**2))    
     
     #partition Hamiltonian
     H_diag = np.diag(np.diag(H))