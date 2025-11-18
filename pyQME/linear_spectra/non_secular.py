(i) fix: bug in calculation of HCE spectra (ii) feat: added option to normalize equilibrium population before fluorescence calculation (iii) feat: implemented possibility to include the imaginary term i*Re(gamma) in the cR eq. pop. (iv) fix: bug in the SpectralDensity.set_time_axis function arising when a unit different than cm was used and the user's variable got modified (v) fix: same as previous point but for various functions in the RelTensor class (vi) feat: implemented option to make the ForsterTensor._calc_rates_freq_domain return the abs and fluo spectra

pyQME/linear_spectra/non_secular.py
@@ -44,7 +44,11 @@ def _calc_exp_K(K_abt,threshold_cond_num=1.02):
             exp_K_abt[:, :, t_idx] = eigvecs_K @ np.diag(np.exp(eigvals_K)) @ eigvecs_K.conj().T        
         #if the condition number is greater than the treshold, calculate the exponential matrix numerically (slower)
         else:
-            exp_K_abt[:, :, t_idx] = expmat(K_abt[:, :, t_idx])
+            try:
+                exp_K_abt[:, :, t_idx] = expmat(K_abt[:, :, t_idx])
+            except:
+                print('t_idx: ',t_idx,'  K_abt[:, :, t_idx].max()):  ',K_abt[:, :, t_idx].max())
+                raise ValueError('Dead here')
     return exp_K_abt
 
         
@@ -922,7 +926,9 @@ class FCE(NonSecularSpectraCalculator):
     def _calc_K_fluo(self):
         "This function calculates and stores the fluorescence lineshape matrix."
         
-        self._calc_K_II()
+        if not hasattr(self,'K_II_ab'):
+            self._calc_K_II()
+        
         self._calc_K_RR()
         self._calc_K_RI()
         K_fluo_abt = self.K_RR_ab-1j*self.K_RI_ab-self.K_II_ab[:,:,np.newaxis]
@@ -1002,10 +1008,7 @@ class HCE(NonSecularSpectraCalculator):
 #             cc_Gamma_tilde_abit += contract('ia,ib,Zabt->abit',c_ia[mask],c_ia[mask],Gamma_tilde_Zabt)
 
         rho_eq_exc_inv = np.linalg.inv(rho_eq_exc)
-        K_RI_ab += contract('ia,ib,abit,bc,ic,id,de->aet',c_ia,c_ia,cc_Gamma_tilde_abit,rho_eq_exc,c_ia,c_ia,rho_eq_exc_inv)
-        
-#         rho_eq_exc_inv = np.linalg.inv(rho_eq_exc)
-#         K_RI_ab += contract('abit,bc,cdi,de->aet',cc_Gamma_tilde_abit,rho_eq_exc,V_abi,rho_eq_exc_inv)
+        K_RI_ab += contract('abit,bc,ic,id,de->aet',cc_Gamma_tilde_abit,rho_eq_exc,c_ia,c_ia,rho_eq_exc_inv)
         
         K_RI_ab *= 1j
         

pyQME/linear_spectra/secular.py
@@ -251,7 +251,7 @@ class SecularSpectraCalculator(SpectraCalculator):
         return freq,abs_OD_i
     
     @add_attributes(spec_type='fluo',units_type='lineshape',spec_components='exciton')
-    def calc_fluo_lineshape_a(self,dipoles,eq_pop=None,freq=None):
+    def calc_fluo_lineshape_a(self,dipoles,eq_pop=None,freq=None,normalize_eqpop=True):
         """Compute fluorescence spectrum.
 
         Arguments
@@ -268,7 +268,7 @@ class SecularSpectraCalculator(SpectraCalculator):
         FL: np.array(dtype = np.float), shape = (freq.size)
             fluorescence intensity.
             units: same as dipoles^2"""
-        self._calc_time_fluo_a(dipoles,eq_pop=eq_pop)
+        self._calc_time_fluo_a(dipoles,eq_pop=eq_pop,normalize_eqpop=normalize_eqpop)
                
         self.fluo_lineshape_a = np.zeros([self.dim,self.freq.size])
         for a in range(self.dim):
@@ -298,7 +298,7 @@ class SecularSpectraCalculator(SpectraCalculator):
         if not self.include_xi_imag:
             self.xi_at_fluo.imag = 0.
     
-    def _calc_time_fluo_a(self,dipoles,eq_pop=None):
+    def _calc_time_fluo_a(self,dipoles,eq_pop=None,normalize_eqpop=True):
         """This function calculates and stores the single-exciton contribution to the fluorescence spectrum in the time domain.
         
         Arguments
@@ -325,8 +325,9 @@ class SecularSpectraCalculator(SpectraCalculator):
 
         if eq_pop is None:
             eq_pop = self.rel_tensor.get_eq_pop_fluo()
-        Z = (eq_pop*self.excd2).sum()
-        eq_pop = eq_pop/Z
+        if normalize_eqpop:
+            Z = (eq_pop*self.excd2).sum()
+            eq_pop = eq_pop/Z
         self.eq_pop = eq_pop
         
         #compute the spectra in the time domain for each exciton without summing up

pyQME/pump_probe.py
@@ -271,7 +271,7 @@ class PumpProbeCalculator():
         W_GSB_a = -W_GSB_a
         
         #SE LINESHAPE
-        _,W_SE_a = self.lin_spec_calculator.calc_fluo_lineshape_a(dipoles,eq_pop=np.ones(self.dim_single),freq=self.freq)
+        _,W_SE_a = self.lin_spec_calculator.calc_fluo_lineshape_a(dipoles,eq_pop=np.ones(self.dim_single),freq=self.freq,normalize_eqpop=False)
         W_SE_a = -W_SE_a
 
         #ESA LINESHAPE

pyQME/spectral_density.py
@@ -101,6 +101,9 @@ class SpectralDensity():
             units in which the time_axis is provided
             can be 'cm', 'ps' or 'fs'"""
 
+        #create a copy to avoid overwriting
+        time_axis=time_axis.copy()
+        
         #convert time_axis to cm
         if units=='ps':
             time_axis *= wn2ips
@@ -159,12 +162,12 @@ class SpectralDensity():
     def _symm_freqs(self):
         "This function creates symmetrical freq axis and spectral densities."
 
-        if self.w[0] == 0:
-            self.omega = np.concatenate((-self.w[:1:-1],self.w))
-            self.Cw = np.asarray([(np.concatenate((-SD[:1:-1],SD))) for SD in self.SD])
-        else:
-            self.omega = np.concatenate((-self.w[::-1],self.w))
-            self.Cw = np.asarray([np.concatenate((-SD[::-1],SD)) for SD in self.SD])
+        # if self.w[0] == 0:
+        #     self.omega = np.concatenate((-self.w[:1:-1],self.w))
+        #     self.Cw = np.asarray([(np.concatenate((-SD[:1:-1],SD))) for SD in self.SD])
+        # else:
+        self.omega = np.concatenate((-self.w[::-1],self.w))
+        self.Cw = np.asarray([np.concatenate((-SD[::-1],SD)) for SD in self.SD])
         pass
 
     def _gen_spline_repr(self,imag=True,derivs=False):

pyQME/tensors/markov/forster.py
@@ -64,7 +64,7 @@ class ForsterTensor(RelTensorMarkov):
         
         self.rates = self.transform(rates)
         
-    def _calc_rates_freq_domain(self,*args,**kwargs):
+    def _calc_rates_freq_domain(self,*args,return_spec=False,**kwargs):
             """This function computes the Forster energy transfer rates by directly calculating the overlap between the fluorescence spectrum of the donor and the absorption spectrum of the acceptor."""
 
             lin_spec = SecularSpectraCalculator(self,*args,**kwargs)
@@ -90,6 +90,9 @@ class ForsterTensor(RelTensorMarkov):
             rates[np.diag_indices_from(rates)] = -np.sum(rates,axis=0)
 
             self.rates = rates #self.transform(rates)
+            
+            if return_spec:
+                return w,OD_a,FL_a
 
     
     def _calc_tensor(self):

pyQME/tensors/relaxation_tensor.py
@@ -372,6 +372,9 @@ class RelTensor():
         if propagation_mode == 'exp_then_eig' and t_switch_exp_to_eig is None:
             raise ValueError('You must input t_switch_exp_to_eig')
         
+        #create a copy to avoid overwriting
+        t=t.copy()
+        
         if units == 'ps':
             t = t*wn2ips
             if t_switch_exp_to_eig is not None:
@@ -448,6 +451,9 @@ class RelTensor():
 
         if propagation_mode == 'exp_then_eig' and t_switch_exp_to_eig is None:
             raise ValueError('You must input t_switch_exp_to_eig')
+        
+        #create a copy to avoid overwriting
+        t=t.copy()
         
         if units == 'ps':
             t = t*wn2ips
@@ -816,13 +822,16 @@ class RelTensorMarkov(RelTensor):
         
         return RTen_secular
     
-    def _calc_eq_pop_fluo(self,include_deph=False,include_lamb=True,normalize=False,include_deph_real=False):
+    def _calc_eq_pop_fluo(self,include_deph_imag=False,include_lamb=True,normalize=False,include_deph_real=False):
         """This function computes and stores the Boltzmann equilibrium population for fluorescence intensity.
         
         Arguments
         -------
-        include_deph: Bool
+        include_deph_imag: Bool
             if True, the energies used for the calculation of the eq. pop. will be shifted by the imaginary part of the dephasing
+            if False, the energies are not shifted
+        include_deph_real: Bool
+            if True, the energies used for the calculation of the eq. pop. will be shifted by the real part of the dephasing, multiplied by the imaginary unit i
             if False, the energies are not shifted
             
         include_lamb: Bool
@@ -839,7 +848,7 @@ class RelTensorMarkov(RelTensor):
             ene = ene + 1j*0
         if include_lamb:
             ene -= self.get_lambda_a()
-        if include_deph:
+        if include_deph_imag:
             ene += self.get_dephasing().imag
         if include_deph_real:
             ene -= 1j*self.get_dephasing().real
@@ -847,8 +856,8 @@ class RelTensorMarkov(RelTensor):
         ene -= ene.min()
         
         boltz = np.exp(-ene*self.specden.beta)
-        if include_deph_real:
-            boltz=boltz.real
+        # if include_deph_real:
+        #     boltz=boltz.real
 
         #the populations are not normalized because the normalization must be done taking into account also of dipoles, which is managed by the SpectraCalculator 
         if normalize: